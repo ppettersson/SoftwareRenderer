cff74b7fe5bf7d58d1f96f6946c0f8d21a8b8bd2
Peter Pettersson <peter.christer.pettersson@gmail.com>
2011-08-15 22:05:29 +0200

    Default C versions of the blend functions to include the alpha channel.

--- Blend.cpp
+++ Blend.cpp
@@ -1,5 +1,13 @@
 #include "SoftwareRenderer.h"
 
+// Sometimes we're really using XRGB, and then it's unnecessary to operate on the alpha channel.
+//   Likewise, sometimes the alpha is only used for the blend operation and we're not really 
+// interested in the resulting alpha channel.
+//   This is only useful for the C version, since the assembler versions use SIMD operations
+// that work on all the channels at the same time.
+#define INCLUDE_ALPHA_CHANNEL
+
+
 dword BlendNormal1_C(dword src, dword dst)
 {
   // Arg1 * alpha + Arg2 * (1 - alpha)
@@ -11,15 +19,21 @@ dword BlendNormal1_C(dword src, dword dst)
         dr  = (dst & kColorRed)   >> 16,
         dg  = (dst & kColorGreen) >>  8,
         db  = (dst & kColorBlue)  >>  0,
-        //da  = (dst & kColorAlpha) >> 24,
+#ifdef INCLUDE_ALPHA_CHANNEL
+        da  = (dst & kColorAlpha) >> 24,
+#endif
 
         inv = 255 - sa,
 
         r   = (sr * sa + dr * inv) >> 8,
         g   = (sg * sa + dg * inv) >> 8,
         b   = (sb * sa + db * inv) >> 8;
-
+#ifdef INCLUDE_ALPHA_CHANNEL
+  dword a   = sa + (da * inv) >> 8;
+  return Color((byte)r, (byte)g, (byte)g, (byte)a);
+#else
   return Color((byte)r, (byte)g, (byte)b);
+#endif
 }
 
 dword BlendMultiply1_C(dword src, dword dst)
@@ -28,18 +42,26 @@ dword BlendMultiply1_C(dword src, dword dst)
   dword sr  = (src & kColorRed)   >> 16,
         sg  = (src & kColorGreen) >>  8,
         sb  = (src & kColorBlue)  >>  0,
-        //sa  = (src & kColorAlpha) >> 24,
+#ifdef INCLUDE_ALPHA_CHANNEL
+        sa  = (src & kColorAlpha) >> 24,
+#endif
 
         dr  = (dst & kColorRed)   >> 16,
         dg  = (dst & kColorGreen) >>  8,
         db  = (dst & kColorBlue)  >>  0,
-        //da  = (dst & kColorAlpha) >> 24,
+#ifdef INCLUDE_ALPHA_CHANNEL
+        da  = (dst & kColorAlpha) >> 24,
+#endif
 
         r   = (sr * dr) >> 8,
         g   = (sg * dg) >> 8,
         b   = (sb * db) >> 8;
-
+#ifdef INCLUDE_ALPHA_CHANNEL
+  dword a   = (sa * da) >> 8;
+  return Color((byte)r, (byte)g, (byte)g, (byte)a);
+#else
   return Color((byte)r, (byte)g, (byte)b);
+#endif
 }
 
 dword BlendAdditive1_C(dword src, dword dst)
@@ -48,18 +70,26 @@ dword BlendAdditive1_C(dword src, dword dst)
   dword sr  = (src & kColorRed)   >> 16,
         sg  = (src & kColorGreen) >>  8,
         sb  = (src & kColorBlue)  >>  0,
-        //sa  = (src & kColorAlpha) >> 24,
+#ifdef INCLUDE_ALPHA_CHANNEL
+        sa  = (src & kColorAlpha) >> 24,
+#endif
 
         dr  = (dst & kColorRed)   >> 16,
         dg  = (dst & kColorGreen) >>  8,
         db  = (dst & kColorBlue)  >>  0,
-        //da  = (dst & kColorAlpha) >> 24,
+#ifdef INCLUDE_ALPHA_CHANNEL
+        da  = (dst & kColorAlpha) >> 24,
+#endif
 
         r   = Min(sr + dr, (dword)255),
         g   = Min(sg + dg, (dword)255),
         b   = Min(sb + db, (dword)255);
-
+#ifdef INCLUDE_ALPHA_CHANNEL
+  dword a   = Min(sa + da, (dword)255);
+  return Color((byte)r, (byte)g, (byte)g, (byte)a);
+#else
   return Color((byte)r, (byte)g, (byte)b);
+#endif
 }
 
 dword BlendSubtractive1_C(dword src, dword dst)
@@ -68,18 +98,26 @@ dword BlendSubtractive1_C(dword src, dword dst)
   dword sr  = (src & kColorRed)   >> 16,
         sg  = (src & kColorGreen) >>  8,
         sb  = (src & kColorBlue)  >>  0,
-        //sa  = (src & kColorAlpha) >> 24,
+#ifdef INCLUDE_ALPHA_CHANNEL
+        sa  = (src & kColorAlpha) >> 24,
+#endif
 
         dr  = (dst & kColorRed)   >> 16,
         dg  = (dst & kColorGreen) >>  8,
         db  = (dst & kColorBlue)  >>  0,
-        //da  = (dst & kColorAlpha) >> 24,
+#ifdef INCLUDE_ALPHA_CHANNEL
+        da  = (dst & kColorAlpha) >> 24,
+#endif
 
         r   = (dword)Max((int)dr - (int)sr, 0),
         g   = (dword)Max((int)dg - (int)sg, 0),
         b   = (dword)Max((int)db - (int)sb, 0);
-
+#ifdef INCLUDE_ALPHA_CHANNEL
+  dword a   = (dword)Max((int)da - (int)sa, 0);
+  return Color((byte)r, (byte)g, (byte)g, (byte)a);
+#else
   return Color((byte)r, (byte)g, (byte)b);
+#endif
 }
 
 dword BlendScreen1_C(dword src, dword dst)
@@ -88,18 +126,26 @@ dword BlendScreen1_C(dword src, dword dst)
   dword sr  = (src & kColorRed)   >> 16,
         sg  = (src & kColorGreen) >>  8,
         sb  = (src & kColorBlue)  >>  0,
-        //sa  = (src & kColorAlpha) >> 24,
+#ifdef INCLUDE_ALPHA_CHANNEL
+        sa  = (src & kColorAlpha) >> 24,
+#endif
 
         dr  = (dst & kColorRed)   >> 16,
         dg  = (dst & kColorGreen) >>  8,
         db  = (dst & kColorBlue)  >>  0,
-        //da  = (dst & kColorAlpha) >> 24,
+#ifdef INCLUDE_ALPHA_CHANNEL
+        da  = (dst & kColorAlpha) >> 24,
+#endif
 
         r   = Clamp((int)(sr + dr - ((sr * dr) >> 8)), (int)0, (int)255),  // ToDo: underflow...
         g   = Clamp((int)(sg + dg - ((sg * dg) >> 8)), (int)0, (int)255),
         b   = Clamp((int)(sb + db - ((sb * db) >> 8)), (int)0, (int)255);
-
+#ifdef INCLUDE_ALPHA_CHANNEL
+  dword a   = Clamp((int)(sa + da - ((sa * da) >> 8)), (int)0, (int)255);
+  return Color((byte)r, (byte)g, (byte)g, (byte)a);
+#else
   return Color((byte)r, (byte)g, (byte)b);
+#endif
 }
 
 dword BlendLighten1_C(dword src, dword dst)
@@ -109,18 +155,26 @@ dword BlendLighten1_C(dword src, dword dst)
   dword sr  = (src & kColorRed)   >> 16,
         sg  = (src & kColorGreen) >>  8,
         sb  = (src & kColorBlue)  >>  0,
-        //sa  = (src & kColorAlpha) >> 24,
+#ifdef INCLUDE_ALPHA_CHANNEL
+        sa  = (src & kColorAlpha) >> 24,
+#endif
 
         dr  = (dst & kColorRed)   >> 16,
         dg  = (dst & kColorGreen) >>  8,
         db  = (dst & kColorBlue)  >>  0,
-        //da  = (dst & kColorAlpha) >> 24,
+#ifdef INCLUDE_ALPHA_CHANNEL
+        da  = (dst & kColorAlpha) >> 24,
+#endif
 
         r   = Max(sr, dr),
         g   = Max(sg, dg),
         b   = Max(sb, db);
-
+#ifdef INCLUDE_ALPHA_CHANNEL
+  dword a   = Max(sa, da);
+  return Color((byte)r, (byte)g, (byte)g, (byte)a);
+#else
   return Color((byte)r, (byte)g, (byte)b);
+#endif
 }
 
 dword BlendDarken1_C(dword src, dword dst)
@@ -130,18 +184,26 @@ dword BlendDarken1_C(dword src, dword dst)
   dword sr  = (src & kColorRed)   >> 16,
         sg  = (src & kColorGreen) >>  8,
         sb  = (src & kColorBlue)  >>  0,
-        //sa  = (src & kColorAlpha) >> 24,
+#ifdef INCLUDE_ALPHA_CHANNEL
+        sa  = (src & kColorAlpha) >> 24,
+#endif
 
         dr  = (dst & kColorRed)   >> 16,
         dg  = (dst & kColorGreen) >>  8,
         db  = (dst & kColorBlue)  >>  0,
-        //da  = (dst & kColorAlpha) >> 24,
+#ifdef INCLUDE_ALPHA_CHANNEL
+        da  = (dst & kColorAlpha) >> 24,
+#endif
 
         r   = Min(sr, dr),
         g   = Min(sg, dg),
         b   = Min(sb, db);
-
+#ifdef INCLUDE_ALPHA_CHANNEL
+  dword a   = Min(sa, da);
+  return Color((byte)r, (byte)g, (byte)g, (byte)a);
+#else
   return Color((byte)r, (byte)g, (byte)b);
+#endif
 }
 
 void BlendNormal_C(dword *src, dword *dst, dword num)
