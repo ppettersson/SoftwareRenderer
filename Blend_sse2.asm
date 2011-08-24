0db1205692d17a17194d378ab100fe980b2acb94
Peter Pettersson <peter.christer.pettersson@gmail.com>
2011-08-24 20:03:23 +0200

    SSE2 version of BlendOver, supports arbitrary number of pixels.

--- Blend_sse2.asm
+++ Blend_sse2.asm
@@ -9,8 +9,11 @@ constant_256_words  times 8  dw 256
 ; Code
 section .text
 
+; #define R_SHUFFLE_D( x, y, z, w ) (( (w) & 3 ) << 6 | ( (z) & 3 ) << 4 | ( (y) & 3 ) << 2 | ( (x) & 3 ))
+
+
 ; Function helper macros
-%macro prologue 0
+%macro Prologue 0
   ; Set up stack frame.
   push        ebp
   mov         ebp, esp
@@ -19,7 +22,7 @@ section .text
   pushad
 %endmacro
 
-%macro epilogue 0
+%macro Epilogue 0
   ; Restore all clobbered registers.
   popad
 
@@ -28,40 +31,15 @@ section .text
   ret
 %endmacro
 
-; #define R_SHUFFLE_D( x, y, z, w ) (( (w) & 3 ) << 6 | ( (z) & 3 ) << 4 | ( (y) & 3 ) << 2 | ( (x) & 3 ))
-
-; void BlendOver_SSE2(dword *src, dword *dst, dword num)
-%define src   ebp + 8
-%define dst   ebp + 12
-%define num   ebp + 16
-align 16
-_BlendOver_SSE2:
-  prologue
-
-  ; Get the arguments into registers.
-  mov         esi, [src]
-  mov         edi, [dst]
-  mov         ecx, [num]
-
-  ; We'll work in 128 bit so we need to handle the special case if it isn't aligned.
-  mov         edx, ecx
-  and         edx, 3            ; edx = number of left over pixels to handle.
-  shr         ecx, 2            ; ecx = num / 4
-  jz BlendOver_SSE2_HandleLeftOvers
-
-  pxor        xmm0, xmm0        ; xmm0 = zero
-
-  ; The inner loop that does the blend operation, 4 pixels at a time.
-.loop
-  ; Grab the source pixels.
-  ; Unfortunately we can't guarantee that the source or destination address are
-  ; properly aligned, so we'll have to use the slower reads here.
-  movdqu      xmm1, [esi]       ; xmm1 = [src0][src1][src2][src3]
-
-  ; Grab the destination pixels and unpack into two registers with words per
-  ; component. This expansion is necessary to perform the multiplication
-  ; without loosing precision.
-  movdqu      xmm2, [edi]       ; xmm2 = [dst0][dst1][dst2][dst3]
+; Perform composite over with premultiplied alpha.
+; xmm0 should hold zeros.
+; xmm1 should hold up to 4 pixels of source pixels.
+; xmm2 should hold up to 4 pixels of destination pixels.
+; The result will be left in xmm1.
+; The following registers will be clobbered: xmm3-5
+%macro BlendOverOp 0
+  ; Unpack into two registers with words per component. This expansion is
+  ; necessary to perform the multiplication without loosing precision.
   movdqa      xmm3, xmm2        ; xmm3 = [dst0][dst1][dst2][dst3]
   punpcklbw   xmm2, xmm0        ; xmm2 = [dst0][dst1]
   punpckhbw   xmm3, xmm0        ; xmm3 = [dst2][dst3]
@@ -101,23 +79,101 @@ _BlendOver_SSE2:
 
   ; Add with the source pixels.
   paddusb     xmm1, xmm2        ; xmm1 = src + dst * src.a
+%endmacro
+
+
+; void BlendOver_SSE2(dword *src, dword *dst, dword num)
+%define src   ebp + 8
+%define dst   ebp + 12
+%define num   ebp + 16
+align 16
+_BlendOver_SSE2:
+  Prologue
+
+  ; Get the arguments into registers.
+  mov         esi, [src]
+  mov         edi, [dst]
+  mov         ecx, [num]
+
+  ; We'll need a register with zeros when unpacking.
+  pxor        xmm0, xmm0        ; xmm0 = zero
+
+  ; We'll work in 128 bit so we need to handle the special case if it isn't aligned.
+  mov         edx, ecx
+  and         edx, 3            ; edx = number of left over pixels to handle.
+  shr         ecx, 2            ; ecx = num / 4
+  jz BlendOver_SSE2_HandleLeftOvers
+
+  ; The inner loop that does the blend operation, 4 pixels at a time.
+;align16 - ToDo: not sure if it's worth aligning the mainloop as well?
+.loop0
+  ; Grab the source pixels.
+  ; Unfortunately we can't guarantee that the source or destination address are
+  ; properly aligned, so we'll have to use the slower reads here.
+  movdqu      xmm1, [esi]       ; xmm1 = [src0][src1][src2][src3]
+
+  ; Grab the destination pixels.
+  movdqu      xmm2, [edi]       ; xmm2 = [dst0][dst1][dst2][dst3]
+
+  ; Blend up to four pixels.
+  BlendOverOp
 
   ; Copy the result back into memory, unaligned.
   movdqu      [edi], xmm1
 
   ; Move on to the next 4 pixels.
-  add         edi, 16
   add         esi, 16
+  add         edi, 16
 
   dec         ecx
-  jnz         .loop
+  jnz         .loop0
 
-  ; Handle the extra odd pixel if there was one.
 BlendOver_SSE2_HandleLeftOvers:
+  ; Handle the extra odd pixels if there are any.
   cmp         edx, 0
   jz          BlendOver_SSE2_Done
 
-;  !!!ToDo!!!
+  ; We have 1-3 pixels left to do, manually fill the source and destination
+  ; registers and then do the blend operation on all 4 pixels even if it only
+  ; happens to be one.
+  mov         ecx, edx        ; Preserve the count to use it when storing the result.
+  mov         eax, edi        ; Preserve the destination as well.
+.loop1
+  ; Copy pixel data from memory.
+  movd        xmm3, [esi]
+  movd        xmm4, [edi]
+
+  ; Shift previous pixel up a step if there was one.
+  ; Note that these shift operations work on whole bytes, not bits.
+  pslldq       xmm1, 4
+  pslldq       xmm2, 4
+  ; Merge the result from previous pixels.
+  por         xmm1, xmm3
+  por         xmm2, xmm4
+
+  ; Move to the next pixel.
+  add         esi, 4
+  add         edi, 4
+
+  dec         ecx
+  jnz         .loop1
+
+  ; Blend up to four pixels.
+  BlendOverOp
+
+  mov         ecx, edx
+.loop2
+  ; Move to the previous pixel.
+  sub         edi, 4
+
+  ; Copy pixel data to memory.
+  movd        [edi], xmm1
+
+  ; Shift previous pixel down a step if there was one.
+  psrldq       xmm1, 4
+
+  dec         ecx
+  jnz         .loop2
 
 BlendOver_SSE2_Done:
-  epilogue
+  Epilogue
